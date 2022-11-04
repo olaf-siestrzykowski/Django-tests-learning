78191223e6d325e8e8a28ae545ca3d6a02007c38
olfis <olfis@o2.pl>
2022-11-04 15:22:22 +0100

    test learning

---
+++ budget/tests/test_views.py
@@ -0,0 +1,44 @@
+from django.test import TestCase, Client
+#from budget.views import
+from django.urls import reverse
+from budget.models import Project, Category, Expense
+import json
+
+
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.list_url = reverse('list')
+        self.detail_url = reverse('detail', args=['project1'])
+        self.project1 = Project.objects.create(
+            name='project1',
+            budget=10000
+        )
+
+    def test_project_GET(self):
+        response = self.client.get(self.list_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'budget/project-list.html')
+
+    def test_detail_GET(self):
+        response = self.client.get(self.detail_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'budget/project-detail.html')
+
+    def test_detail_POST_new_expense(self):
+        Category.objects.create(
+            project=self.project1,
+            name='development'
+        )
+        response = self.client.post(self.detail_url, {
+            'title': 'expense1',
+            'amount': 1000,
+            'category': 'development'
+        })
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(self.project1.expenses.first().title, 'expense1')
+
+    def test_detail_POST_no_data(self):
+        response = self.client.post(self.detail_url)
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(self.project1.expenses.count(), 0)
